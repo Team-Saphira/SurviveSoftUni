0acea0397d96954b3645d7c83a0e508741e5e52e
vanncho <van_cho@abv.bg>
2016-07-03 17:06:18 +0300

    Add field to ImageLoader to show bullet image on screen.

--- src/game/sprites/ImageLoader.java
+++ src/game/sprites/ImageLoader.java
@@ -1,9 +1,9 @@
 package game.sprites;
 
-
 import javafx.scene.image.Image;
 
 public class ImageLoader {
     public static Image zombieImage = new Image(ImageLoader.class.getResourceAsStream("/game/resources/zombie.png"));
     public static Image playerImage = new Image(ImageLoader.class.getResourceAsStream("/game/resources/survivor-move_handgun.png"));
+    public static Image bulletImage = new Image(ImageLoader.class.getResourceAsStream("/game/resources/bullet.png"));
 }
